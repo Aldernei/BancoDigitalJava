07a71c2f1ac2a0007c711293cfc159188ac54594
Aldernei <josejunior3979@gmail.com>
2022-07-17 20:30:38 -0300

    banco digital java first commit

---
+++ Cliente.java
@@ -0,0 +1,18 @@
+package bancoDigitalJava;
+
+/**
+ *
+ * @author Aldernei
+ */
+public class Cliente {
+    
+    public String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
